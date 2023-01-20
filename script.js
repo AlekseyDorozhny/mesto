2b36be593299f68473be670b755ab9cabdf7e966
Aleksey Zhirov <AlekseyDorozhny@yandex.ru>
2023-01-20 11:54:39 +0700

    fix: подправлены замечания ревьювера

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* Открытие/закрытие pop-up */
 
-let popupOpenButton = document.querySelector('.profile__editButton');
-let popupCloseButton = document.querySelector('.popup__closeButton');
+let popupOpenButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup');
 
 function popupOpen() {
@@ -17,7 +17,7 @@ popupCloseButton.addEventListener('click', popupClose);
 
 /* Изменение имени и активности профиля */
 
-let formElement = document.querySelector('.popup__container')
+let formElement = document.querySelector('.popup__form')
 let nameInput = document.querySelector('.popup__input_name')
 let activityInput = document.querySelector('.popup__input_activity')
 let profileName = document.querySelector('.profile__name')
