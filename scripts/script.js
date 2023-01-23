eafe17faa78ceed7dcf230c7288361114b2aea16
Aleksey Zhirov <AlekseyDorozhny@yandex.ru>
2023-01-23 16:28:03 +0700

    fix: исправлены замечания ревьювера

--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,20 @@
 let popupOpenButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form')
+let nameInput = document.querySelector('.popup__input_type_name')
+let activityInput = document.querySelector('.popup__input_type_activity')
+let profileName = document.querySelector('.profile__name')
+let profileActivity = document.querySelector('.profile__activity')
 
 function popupOpen() {
-  popup.classList.add('popup_opened')
+  nameInput.setAttribute('value', profileName.textContent);
+  activityInput.setAttribute('value', profileActivity.textContent);
+  popup.classList.add('popup_opened');
 }
 
 function popupClose() {
-  popup.classList.remove('popup_opened')
+  popup.classList.remove('popup_opened');
 }
 
 popupOpenButton.addEventListener('click', popupOpen);
@@ -17,14 +24,7 @@ popupCloseButton.addEventListener('click', popupClose);
 
 /* Изменение имени и активности профиля */
 
-let formElement = document.querySelector('.popup__form')
-let nameInput = document.querySelector('.popup__input_name')
-let activityInput = document.querySelector('.popup__input_activity')
-let profileName = document.querySelector('.profile__name')
-let profileActivity = document.querySelector('.profile__activity')
 
-nameInput.setAttribute('value', profileName.textContent);
-activityInput.setAttribute('value', profileActivity.textContent);
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
