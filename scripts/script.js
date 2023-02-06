b32194c4a4a09b7502189bcb5a7b912c2c567394
Aleksey Zhirov <AlekseyDorozhny@yandex.ru>
2023-02-06 14:16:00 +0700

    законченный черновик

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+/*Для ревьювера: Я знаю, что мой код жутко не эфеективный, но к сожалению, я не знаю, как сделать его более легким и "правильным", по этому оставлю до первого ревью  все  как есть, потому что "оно" хотя бы работает :) */
+
 /*Перменные попапа профиля и самого профиля*/
 let profilePopupOpenButton = document.querySelector('.profile__edit-button');
 let profilePopupCloseButton = document.querySelector('.popup__close-button_area_profile');
@@ -44,11 +46,18 @@ const initialCards = [
 const cardNameInput = document.querySelector('.popup__input_type_card-name');
 const cardSrcInput = document.querySelector('.popup__input_type_card-Src');
 let cardFormElement = document.querySelector('.popup__form_type_card');
-const element = document.querySelector('.element');
-const elements = document.querySelector('.elements');
+let element = document.querySelector('.element');
+let allElements = document.querySelectorAll('.element');
+let elements = document.querySelector('.elements');
 let like = document.querySelectorAll('.element__like');
+let trash = document.querySelectorAll('.element__trash');
 
-
+/*Переменные попапа просмотра карточки */
+const cardPopupView = document.querySelector('.popup_type_image');
+const cardView = cardPopupView.querySelector('.image-popup');
+const cardPopupViewCloseButton = cardPopupView.querySelector('.popup__close-button_area_image');
+const cardViewName = cardPopupView.querySelector('.image-popup__name');
+const cardImage = cardPopupView.querySelector('.image-popup__image');
 
 
 /* Открытие/закрытие pop-up профиля */
@@ -79,16 +88,16 @@ profileFormElement.addEventListener('submit', handleFormSubmit);
 
 /* Открытие/закрытие pop-up добавления карточки*/
 
-function cardPopupOpen() {
+function addCardPopupOpen() {
   cardPopup.classList.add('popup_opened');
 }
 
-function cardPopupClose() {
+function addCardPopupClose() {
   cardPopup.classList.remove('popup_opened');
 }
 
-addCardButton.addEventListener('click', cardPopupOpen);
-cardPopupCloseButton.addEventListener('click', cardPopupClose);
+addCardButton.addEventListener('click', addCardPopupOpen);
+cardPopupCloseButton.addEventListener('click', addCardPopupClose);
 
 
 /* Первичное заполнения блоков element__image */
@@ -116,9 +125,19 @@ function addNewCard (evt) {
   newCard.unshift({name: cardNameInput.value, link: cardSrcInput.value});
 
   let newElement = element.cloneNode(true);
-  console.log(newElement);
- const newElementImage = newElement.querySelector('img');
+  const newElementImage = newElement.querySelector('img');
   const newElementName = newElement.querySelector('.element__name');
+
+  let newElementTrash = newElement.querySelector('.element__trash');
+  let newElementLike = newElement.querySelector('.element__like');
+
+  newElementLike.addEventListener('click', function(evt) {
+  evt.target.classList.toggle('element__like_status_active');
+  });
+
+  newElementTrash.addEventListener('click', function() {
+    newElement.remove();
+  })
 
 
   newElementImage.src = newCard[0].link;
@@ -131,12 +150,40 @@ function addNewCard (evt) {
 
 cardFormElement.addEventListener('submit', addNewCard);
 
-/* Лайки */
+/* Лайки старых карточек*/
 
-like.addEventListener ('click', function(evt) {
-  evt.target.classList.toggle('element__like_status_active');
+[].forEach.call(like, function(item) {
+  item.addEventListener('click', function(evt) {
+    evt.target.classList.toggle('element__like_status_active');
+  });
+}) ;
+
+/* Удаление старых карточек */
+
+trash.forEach(function(trash, i) {
+  trash.addEventListener('click', function() {
+    allElements[i].remove();
+  })
 });
 
+/* Попап просмотра карточек */
 
 
-console.log(like);
+  let elmntImage = document.querySelectorAll('.element__image');
+  let elmntImageName = document.querySelectorAll('.element__name');
+
+
+  elmntImage.forEach(function(elmnt, i) {
+    elmnt.addEventListener('click', function() {
+      cardImage.src = elmntImage[i].src;
+      cardViewName.textContent = elmntImageName[i].textContent;
+      cardPopupView.classList.add('popup_opened');
+    })
+  });
+
+function cardPopupClose() {
+  cardPopupView.classList.remove('popup_opened');
+}
+
+cardPopupViewCloseButton.addEventListener('click', cardPopupClose);
+
